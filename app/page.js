4272a6a0cf86a81c3a805f7526264d322c87b06c
Nurul <njagirdar@virginmedia.com>
2024-02-08 23:22:12 +0000

    ui changes

---
+++ app/page.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-4 bg-black text-white min-h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="card bg-gradient-to-br from-purple-700 to-gray-900">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">21°C</h2>
+            <p>Leeds</p>
+            <p>Thu 8 Feb 2023</p>
+          </div>
+        </div>
+
+        <div className="card bg-gradient-to-br from-gray-700 to-gray-900">
+          <div className="card-body">
+            <h2 className="card-title">3 Days Forecast</h2>
+            <div className="flex justify-between">
+              <span>Monday</span>
+              <span>23°C</span>
+            </div>
+            <div className="flex justify-between">
+              <span>Tuesday</span>
+              <span>11°C</span>
+            </div>
+            <div className="flex justify-between">
+              <span>Wednesday</span>
+              <span>31°C</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="card bg-gradient-to-br from-gray-700 to-gray-900">
+          <div className="card-body">
+            <h2 className="card-title">Today's Highlights</h2>
+            <div className="flex justify-between">
+              <span>Humidity</span>
+              <span>84%</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
